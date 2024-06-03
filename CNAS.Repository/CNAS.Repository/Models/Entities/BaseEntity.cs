8f4a11ede6f39331526cf6e842904ca10fd85ff7
Luca Castellaro <luca_castellaro@outlook.com>
2024-06-03 16:02:02 +0200

    Base entity

---
+++ CNAS.Repository/CNAS.Repository/Models/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CNAS.Repository.Models.Entities;
+
+[Serializable]
+[BsonIgnoreExtraElements]
+public abstract record BaseEntity
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    [BsonIgnoreIfDefault]
+    public string Id { get; set; } = default!;
+}
